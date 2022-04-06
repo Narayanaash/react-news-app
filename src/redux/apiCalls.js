ebe0b661e9275cef506417bb304ab759eba31fd0
aash narayan <aash.hepta@gmail.com>
2022-04-06 22:51:41 +0530

    api changed to node server

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -1,12 +1,9 @@
 import axios from 'axios';
 import { getNewsSuccess } from './breakingNewsSlice';
-const apiKey = process.env.REACT_APP_NEWS_API_KEY;
 
 export const fetchBreakingNews = async (dispatch) => {
   try {
-    const res = await axios.get(
-      'https://newsapi.org/v2/top-headlines?country=in&apiKey=' + apiKey
-    );
+    const res = await axios.get('http://reactnewz.herokuapp.com/');
     dispatch(getNewsSuccess(res.data.articles));
   } catch (err) {}
 };
