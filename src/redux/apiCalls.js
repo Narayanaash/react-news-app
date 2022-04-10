204f50321721a43e3cb311587116de0552143239
aash narayan <aash.hepta@gmail.com>
2022-04-10 19:33:18 +0530

    bug fix

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -10,53 +10,49 @@ import { getInnerNewsSuccess } from './innerNewsSlice';
 
 export const fetchBreakingNews = async (dispatch) => {
   try {
-    const res = await axios.get('https://reactnewz.herokuapp.com/headlines');
+    const res = await axios.get('http://localhost:5000/headlines');
     dispatch(getNewsSuccess(res.data.articles));
   } catch (err) {}
 };
 
 export const fetchEntertainmentNews = async (dispatch) => {
   try {
-    const res = await axios.get(
-      'https://reactnewz.herokuapp.com/entertainment'
-    );
+    const res = await axios.get('http://localhost:5000/entertainment');
     dispatch(getEntertainmentNewsSuccess(res.data.articles));
   } catch (err) {}
 };
 
 export const fetchInternationalNews = async (dispatch) => {
   try {
-    const res = await axios.get(
-      'https://reactnewz.herokuapp.com/international'
-    );
+    const res = await axios.get('http://localhost:5000/international');
     dispatch(getInternationalNewsSuccess(res.data.articles));
   } catch (err) {}
 };
 
 export const fetchSportsNews = async (dispatch) => {
   try {
-    const res = await axios.get('https://reactnewz.herokuapp.com/sports');
+    const res = await axios.get('http://localhost:5000/sports');
     dispatch(getSportsNewsSuccess(res.data.articles));
   } catch (err) {}
 };
 
 export const fetchBusinessNews = async (dispatch) => {
   try {
-    const res = await axios.get('https://reactnewz.herokuapp.com/business');
+    const res = await axios.get('http://localhost:5000/business');
     dispatch(getBusinessNewsSuccess(res.data.articles));
   } catch (err) {}
 };
 
 export const fetchPoliticsNews = async (dispatch) => {
   try {
-    const res = await axios.get('https://reactnewz.herokuapp.com/politics');
+    const res = await axios.get('http://localhost:5000/politics');
     dispatch(getPoliticsNewsSuccess(res.data.articles));
   } catch (err) {}
 };
 
 export const fetchGadgetsNews = async (dispatch) => {
   try {
-    const res = await axios.get('https://reactnewz.herokuapp.com/gadgets');
+    const res = await axios.get('http://localhost:5000/gadgets');
     dispatch(getGadgetsNewsSuccess(res.data.articles));
   } catch (err) {}
 };
@@ -64,7 +60,7 @@ export const fetchGadgetsNews = async (dispatch) => {
 // for inner page
 export const fetchInnerNews = async (page, dispatch) => {
   try {
-    const res = await axios.get('https://reactnewz.herokuapp.com/' + page);
+    const res = await axios.get('http://localhost:5000/' + page);
     dispatch(getInnerNewsSuccess(res.data.articles));
   } catch (err) {}
 };
